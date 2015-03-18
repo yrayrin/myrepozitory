35084af37ff0993f9f9082738a4d0e83838701b2
yrayrin <yhra@tut.by>
2015-03-18 18:35:20 +0300

    ss

--- ConsoleApplication3/Program.cs
+++ ConsoleApplication3/Program.cs
@@ -11,6 +11,7 @@ namespace ConsoleApplication3
         static void Main(string[] args)
         {
             Console.WriteLine("Error");
+            Console.WriteLine("Error");
         }
     }
 }
